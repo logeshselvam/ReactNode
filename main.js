53bf5c31ea9e1fa283302aa22e3b7efa78f43415
logeshselvam <45010065+logeshselvam@users.noreply.github.com>
2019-07-31 12:19:57 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,32 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App.js';
+We saw a Job Opening in LinkedIn for the post of Frontend Developer in your company.
 
-ReactDOM.render(<App />, document.getElementById('app'));
+We have 3+ years of experience in Javascript/React.js and we have developed 
+few applications on our own , one of the application is bought by a company which is 
+in Live right now .
+
+Link : http://www.vexamine.com/
+Stack used : 
+- > React js
+- > Node js 
+- > Java 8 
+- > Spring boot
+- > Deployed in AWS 
+
+For about a long time we are looking for a right opportunity, finally we saw this opening which gave us an huge excitment.
+Please let us know if there is any opportunity to be part of your company and we are very much interested to relocate.
+
+Notice period : 2 Months / Immediate Buyout
+
+Stack details are updated in the attached resume.
+
+GitHub: 
+https://github.com/logeshselvam
+https://github.com/vairavan6
+
+Project Link : https://github.com/ForYou-Always/forYou
+
+The above github account is the current project which we are developing on self interest.
+
+We are as a team with 2 members with 4.5 + years of experience in full stack development .
+So I have attched the details about both of us. We have worked as a freelancing developer for 4 projects , 
+even if there is a any opputunity to be part of your company as a offshore employee please let us know .
