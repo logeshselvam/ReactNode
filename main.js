49ab263e7c769e070e146f8a04ad0b37fb15cc97
logeshselvam <45010065+logeshselvam@users.noreply.github.com>
2019-11-18 18:15:32 +0530

    Update main.js

--- main.js
+++ main.js
@@ -3,3 +3,20 @@ import ReactDOM from 'react-dom';
 import App from './App.js';
 
 ReactDOM.render(<App />, document.getElementById('app'));
+
+
+
+I am interested in the Front End Engineer- React Js at Flix Company, as advertised on LinkedIn.I
+am currently employed as a Senior Engineer & Team Lead for IVTL Infoview Technologies pvt ltd , Chennai, India.
+I believe that the skills and experiences I have gained at this position make me an
+ideal candidate for the job of Front end engineer.
+
+As a Senior Engineer , I was responsible for end to end web app
+development and creative Ui Tools for the project. Lead three teams of 10 members each . 
+In my current role , I have been responsible to develop responsive UI in the frontend using
+react js library and Rest api using Node js . As a Team Lead , I have guided my team for the purpose of
+achieveing the teams's goals and to evaluate team members strengths and weaknesses and delegate tasks
+accordingly . 
+
+I have been with my 
+
