f0f9a6308313c64bf5103340934627e0f0dfa3b4
logeshselvam <45010065+logeshselvam@users.noreply.github.com>
2019-11-27 17:12:39 +0530

    Update main.js

--- main.js
+++ main.js
@@ -5,9 +5,27 @@ Hope you are doing good !!!
 
 We saw a several Job Opening in your company.
 
-We have 4.5+ years of experience in Full stack Web development in versatile technologies. 
+About ForYou Tech Company :
+
+ForYou Tech is a group of working professionals and passionate freelancers who has 
+passion towards polygot software development and automation. 
+
+Origin & Moto:
+            Every Passionate software engineer's dream is to become a Software Architect.
+We achieve this dream in ForYou Tech. In a regular job developer will be have narrow scope of development.
+But in ForYou Tech we have an wider opportunity to meet different people in different organisations as a
+technical consultants and automate every possibilities.
+
+We have bunch of passionate engineers experienced in Full stack Web development in versatile technologies. 
 We have developed few applications on our own and we have worked as a freelance developers in 
-multiple projects for various Australia, UK based concerns. 
+multiple projects for various Australia, US based concerns. 
+
+We do this only for the love of programming.
+Since you are a growing startup you have to invest a lot on the training for the new developers and 
+more over if you recruit a full time employee you need to invest more on them .  
+We can be your short term solution . But we will be very happy to work with you in a long term as per your need.
+This model is like AdHoc development team. Based on your need you can utilize us. i.e Payment based on demand.
+We are even ready to do a POC so you can evaluate us more .
 
 What we do & How we Work:
 1. We will collaborate with your Company In-house Team. 
@@ -15,12 +33,10 @@ What we do & How we Work:
 At the start , We will have a business & technical KT meetings with company's Internal dev team.
 After that we will have a brief analysis internally like for 2days. Based on the discussion we will 
 start the development along with your team.
-
 In this type of freelancing collaboration also we will take sole responsibility for the work allotted to us 
 and we will have additional internal backup to complete the work @any cost with Perfection.
 
 Example:
-
 So far we have completed 3 different projects and one is for US Marine in above explained way.
 Since we are available in chennai we can support in-person during the weekends and if needed 
 we can support after 7pm twice in a week.
@@ -38,18 +54,7 @@ Nest, Loopback, Express
 - > Nginx, Docker, Git, Jenkins, SVN ......
 Kindly can you give us few minutes of video meeting
 so that we shall explain in detail about how we work. It will help to know us in a better way.
-Tech Stack details are updated in the attached resume.
-
-
-Note :
-
-We are like 4 passionate developers working as a team . We do this only for the love of programming.
-Since you are a growing startup you have to invest a lot on the training for the new developers and 
-more over if you recruit a full time employee you need to invest more on them .  
-we can be your short term solution . But we will be very happy to work with you in a long term as per your need.
-
-This model is like AdHoc development team.Based on your need you can utilize us. i.e Payment based on demand.
-We are even ready to do a POC so you can evaluate us more .
+Tech Stack and Project details are updated in the attached resume.
 
 GitHub:
 https://github.com/logeshselvam
@@ -58,7 +63,7 @@ Project Links:
 https://github.com/ForYou-Always/forYou
 http://www.vexamine.com - live { client project }
 
-Contact Details 
+ForYou Tech - Contact Persons 
 * Logesh 
 slogeshcs@gmail 
 +91 9566680354
