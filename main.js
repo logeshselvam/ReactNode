e46cc3c42faad1003840ac659f0d632bccf63875
logeshselvam <45010065+logeshselvam@users.noreply.github.com>
2019-11-27 16:00:23 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,71 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App.js';
+Hi Firat Madon,
+     
+Greetings !!! 
+Hope you are doing good !!!
 
-ReactDOM.render(<App />, document.getElementById('app'));
+
+We saw a Job Opening in LinkedIn for the Full Stack Developer position in SPLIT CHENNAI.
+
+We have 4.5+ years of experience in Full stack Web development in versatile technologies. We have developed few applications by ForYouTech - freelancing team and currently we are working in multiple projects for various Australia, UK based concerns with our powerful team. 
+ForYouTech has group of developers who are in interested in Freelancing.
+
+What we do & How we Work:
+
+1. Complete Project take over by ForYou team
+I.e Full Development
+
+ForYouTech will get the specification and business information from the companies and have initial KT sessions. Once we are ok with KT meetings, then ForYou team Will take the full responsibility from the sratch till the end delivery to company.And later bug fixes will be handled by ForYou team. We take sole responsibility.
+
+Sample:
+We have developed a revenue generating application for a US based concern and from the scratch till AWS deployment done by our team.
+
+2. ForYouTech team collaborate with Client Company Onboard Team. i.e part of their development project
+
+At the start , We will have a business & technical KT meetings with company's Internal dev team. After that we will have a brief analysis internally like for 2days. Based on the discussion we will allot developers {includes us also} for the Company. The developers will collaborate with Client companies internal dev team. 
+In this type of work collaboration also we will take sole responsibility for the work allotted to our developers and we will have additional internal backup to complete the work @any cost with Perfection.
+
+Example:
+Currently 2 of our freelancing teams has been deployed in 2 different projects {US Marine projects} in above explained way. We collaborate at weekends too. If at all Necessary our developers will support Inperson.
+
+ForYou tech will take care of confidentiality and security.
+
+Stack, Platforms, Maintanence systems used :
+- > React js
+React Native, Gulp, Webpack, Babel
+- > Node js 
+Nest, Loopback, Express, Typescript
+-> Postgresql, Mongodb
+- > Java 8
+- > Spring boot, Hibernate
+- > AWS Cloud
+- > Nginx, Docker, Git, Jenkins, SVN ......
+
+Kindly can you give us few minutes of video meeting
+so that we shall explain in detail about our effective working strategy and success rates that we have achieved. It will help to know us in a better way.
+
+Tech Stack details are updated in the attached resume.
+
+GitHub:
+https://github.com/logeshselvam
+https://github.com/vairavan6
+
+Project Links:
+https://github.com/ForYou-Always/forYou
+http://www.vexamine.com - live { client project }
+
+
+ForYouTech Team Directors:
+* Logesh 
+slogeshcs@gmail 
++91 9566680354
+
+* Vairavan 
+vairavan.u@gmail.com
++91 9042403354
+
+Looking Forward to hear from you.
+
+
+Thanks & Regards , 
+Logesh S 
+ForYou Tech.
