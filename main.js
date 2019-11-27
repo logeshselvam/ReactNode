4434cce429ce6a58b413d86c9b4dab51388f90e3
logeshselvam <45010065+logeshselvam@users.noreply.github.com>
2019-11-27 16:35:27 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,71 +1,76 @@
-Hi Firat Madon,
+Hi Sir/Madam,
      
 Greetings !!! 
 Hope you are doing good !!!
 
+We saw a several Job Opening in your company.
 
-We saw a Job Opening in LinkedIn for the Full Stack Developer position in SPLIT CHENNAI.
-
-We have 4.5+ years of experience in Full stack Web development in versatile technologies. We have developed few applications by ForYouTech - freelancing team and currently we are working in multiple projects for various Australia, UK based concerns with our powerful team. 
-ForYouTech has group of developers who are in interested in Freelancing.
+We have 4.5+ years of experience in Full stack Web development in versatile technologies. 
+We have developed few applications on our own and we have worked as a freelance developers in 
+multiple projects for various Australia, UK based concerns. 
 
 What we do & How we Work:
+1. We will collaborate with your Company In-house Team. 
 
-1. Complete Project take over by ForYou team
-I.e Full Development
+At the start , We will have a business & technical KT meetings with company's Internal dev team.
+After that we will have a brief analysis internally like for 2days. Based on the discussion we will 
+start the development along with your team.
 
-ForYouTech will get the specification and business information from the companies and have initial KT sessions. Once we are ok with KT meetings, then ForYou team Will take the full responsibility from the sratch till the end delivery to company.And later bug fixes will be handled by ForYou team. We take sole responsibility.
-
-Sample:
-We have developed a revenue generating application for a US based concern and from the scratch till AWS deployment done by our team.
-
-2. ForYouTech team collaborate with Client Company Onboard Team. i.e part of their development project
-
-At the start , We will have a business & technical KT meetings with company's Internal dev team. After that we will have a brief analysis internally like for 2days. Based on the discussion we will allot developers {includes us also} for the Company. The developers will collaborate with Client companies internal dev team. 
-In this type of work collaboration also we will take sole responsibility for the work allotted to our developers and we will have additional internal backup to complete the work @any cost with Perfection.
+In this type of freelancing collaboration also we will take sole responsibility for the work allotted to us 
+and we will have additional internal backup to complete the work @any cost with Perfection.
 
 Example:
-Currently 2 of our freelancing teams has been deployed in 2 different projects {US Marine projects} in above explained way. We collaborate at weekends too. If at all Necessary our developers will support Inperson.
 
-ForYou tech will take care of confidentiality and security.
+So far we have completed 3 different projects and one is for US Marine in above explained way.
+Since we are available in chennai we can support in-person during the weekends and if needed 
+we can support after 7pm twice in a week.
+we will take care of confidentiality and security.
 
 Stack, Platforms, Maintanence systems used :
 - > React js
-React Native, Gulp, Webpack, Babel
+React Native, Gulp, Webpack, Babel , ant d , responsive web design , css flexbox
 - > Node js 
-Nest, Loopback, Express, Typescript
--> Postgresql, Mongodb
-- > Java 8
+Nest, Loopback, Express
+- > Postgresql, Mongodb
+- > TypeScript , Java 8
 - > Spring boot, Hibernate
 - > AWS Cloud
 - > Nginx, Docker, Git, Jenkins, SVN ......
-
 Kindly can you give us few minutes of video meeting
-so that we shall explain in detail about our effective working strategy and success rates that we have achieved. It will help to know us in a better way.
-
+so that we shall explain in detail about how we work. It will help to know us in a better way.
 Tech Stack details are updated in the attached resume.
+
+
+Note :
+
+We are like 4 passionate developers working as a team . We do this only for the love of programming.
+Since you are a growing startup you have to invest a lot on the training for the new developers and 
+more over if you recruit a full time employee you need to invest more on them .  
+we can be your short term solution . But we will be very happy to work with you in a long term as per your need.
+
+This model is like AdHoc development team.Based on your need you can utilize us. i.e Payment based on demand.
+We are even ready to do a POC so you can evaluate us more .
 
 GitHub:
 https://github.com/logeshselvam
 https://github.com/vairavan6
-
 Project Links:
 https://github.com/ForYou-Always/forYou
 http://www.vexamine.com - live { client project }
 
-
-ForYouTech Team Directors:
-* Logesh 
-slogeshcs@gmail 
+Contact Details 
+* Logesh 
+slogeshcs@gmail 
 +91 9566680354
 
-* Vairavan 
+* Vairavan 
 vairavan.u@gmail.com
 +91 9042403354
 
 Looking Forward to hear from you.
 
+Thanks & Regards , 
+Logesh S 
 
-Thanks & Regards , 
-Logesh S 
-ForYou Tech.
+
+
